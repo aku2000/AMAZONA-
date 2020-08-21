3e82bdc67ec6e28b38e8c5ad276adfc9313e3a39
Akansha Agarwal <akurox123@gmail.com>
2020-08-21 11:40:33 +0530

    Render Products

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products: [
+        {
+            name: 'Woman T-shirt',
+            category: 'T-shirts',
+            image: '/images/d1.jpg',
+            price: 1500,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            name: 'Crop top',
+            category: 'T-shirts',
+            image: '/images/d2.jpg',
+            price: 800,
+            brand: 'Mango',
+            rating: 3,
+            numReviews: 32
+        },
+        {
+            name: 'High Waist Jeans',
+            category: 'Jeans',
+            image: '/images/d3.jpg',
+            price: 2500,
+            brand: 'H&M',
+            rating: 4.5,
+            numReviews: 16
+        }
+    ]
+}
