a8a3cd9de1de1da2c65778acc21e66650858f3af
Akansha Agarwal <akurox123@gmail.com>
2020-08-23 14:12:55 +0530

    Create Node Server (Node.js)

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Woman T-shirt',
+            category: 'T-shirts',
+            image: '/images/d1.jpg',
+            price: 1500,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            _id: '2',
+            name: 'Crop top',
+            category: 'T-shirts',
+            image: '/images/d2.jpg',
+            price: 800,
+            brand: 'Mango',
+            rating: 3,
+            numReviews: 32
+        },
+        {
+            _id: '3',
+            name: 'High Waist Jeans',
+            category: 'Jeans',
+            image: '/images/d3.jpg',
+            price: 2500,
+            brand: 'H&M',
+            rating: 4.5,
+            numReviews: 16
+        },
+    ]
+}
